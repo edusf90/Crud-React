7c163b9e7e75f6a0b2aef1a3974dd01b1dc6e218
Eduardo Santana <edusf0@hotmail.com>
2023-01-26 14:19:49 -0300

    atualizado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import TaskContextProvider from './context/TaskContext';
-import Header from './components/Header';
-import TaskForm from './components/TaskForm';
-import TaskList from './components/TaskList';
-import './App.css';
+import TaskContextProvider from './context/TaskContext'
+import Header from './components/Header'
+import TaskForm from './components/TaskForm'
+import TaskList from './components/TaskList'
+import './App.css'
+import React from 'react'
 
 function App() {
   return (
     <TaskContextProvider>
-    <div className="container">
-      <div className="app-wrapper">
-        <Header />
-        <div className="main">
-          <TaskForm />
-          <TaskList />
-          
+      <div className="container">
+        <div className="app-wrapper">
+          <Header />
+          <div className="main">
+            <TaskForm />
+            <TaskList />
+          </div>
         </div>
       </div>
-    </div>
     </TaskContextProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,11 +1,12 @@
 import './styles.css'
+import React from 'react'
 
-function Header (){
-    return (
-        <header className="header">
-          <h1>Lista de Tarefas</h1>
-        </header>
-    );
+function Header() {
+  return (
+    <header className="header">
+      <h1>Lista de Tarefas</h1>
+    </header>
+  )
 }
 
-export default Header;
+export default Header
